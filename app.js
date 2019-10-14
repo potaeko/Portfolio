51fac2a42220d276a8cf468a0f92fd8ecfcd4a1d
Thomas Ko <potaeko@hotmail.com>
2019-10-13 21:59:48 -0700

    fixed Safari layout

--- app.js
+++ app.js
@@ -53,3 +53,49 @@ function pauseUnhover(el){
   el.pause();
   el.currentTime = 0;
 }
+
+/*===============================================
+                   NAVIGATION
+    Show/Hide transparent black navigation
+================================================*/
+$(function(){
+    $(window).scroll(function(){
+        // if scroll top lower than 50px we wwant to hide Nav bar
+        if($(this).scrollTop()<50){
+            //hide nav
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();
+        }
+        else{
+            //show nav
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
+//Smooth scrolling
+$(function(){
+    $("a.smooth-scrolling").click(function(event){
+        //stop open link url
+        event.preventDefault(); 
+        //this = a
+        //get/return id like #about, #work, #team and etc, if clicked 
+        var section = $(this).attr("href");
+        
+        //animate
+        $('html, body').animate({
+            scrollTop: $(section).offset().top-64
+        }, 1250,"easeInOutExpo"); //how long animation will run 1.25sec
+
+    });
+});
+
+//Close mobile menu on click : click menu == click toggle button to close
+$(function(){
+        //class name for a tag in li
+    $(".navbar-collapse ul li a").on("click touch",function(){
+        //class name of toggle button
+        $(".navbar-toggle").click();
+    });
+
+});
